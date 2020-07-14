add3996f90aa6ed390e7d1ff8bd5022ca18a6e5b
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-14 09:45:02 +0700

    add excersice

--- java-cleancode-refactoring-exercise-master/src/TennisGame.java
+++ java-cleancode-refactoring-exercise-master/src/TennisGame.java
@@ -1,5 +1,20 @@
 public class TennisGame {
 
+    public static final String LOVE_ALL = "Love-All";
+    public static final String FIFTEEN_ALL = "Fifteen-All";
+    public static final String THIRTY_ALL = "Thirty-All";
+    public static final String FORTY_ALL = "Forty-All";
+    public static final String DEUCE = "Deuce";
+    public static final String ADVANTAGE_PLAYER_1 = "Advantage player1";
+    public static final String ADVANTAGE_PLAYER_2 = "Advantage player2";
+    public static final String WIN_FOR_PLAYER_1 = "Win for player1";
+    public static final String WIN_FOR_PLAYER_2 = "Win for player2";
+    public static final String MINUS = "-";
+    public static final String LOVE = "Love";
+    public static final String FIFTEEN = "Fifteen";
+    public static final String THIRTY = "Thirty";
+    public static final String FORTY = "Forty";
+
     public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
         String score = "";
         int tempScore=0;
@@ -8,19 +23,20 @@ public class TennisGame {
             switch (m_score1)
             {
                 case 0:
-                    score = "Love-All";
+                    score = LOVE_ALL;
                     break;
+
                 case 1:
-                    score = "Fifteen-All";
+                    score = FIFTEEN_ALL;
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    score = THIRTY_ALL;
                     break;
                 case 3:
-                    score = "Forty-All";
+                    score = FORTY_ALL;
                     break;
                 default:
-                    score = "Deuce";
+                    score = DEUCE;
                     break;
 
             }
@@ -28,30 +44,37 @@ public class TennisGame {
         else if (m_score1>=4 || m_score2>=4)
         {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            boolean isResultEqual1 = minusResult == 1;
+            if (isResultEqual1) score = ADVANTAGE_PLAYER_1;
+            else {
+                boolean isResultEqualMinus1 = minusResult == -1;
+                if (isResultEqualMinus1) score = ADVANTAGE_PLAYER_2;
+                else {
+                    boolean isResultLargerOrEqual2 = minusResult >= 2;
+                    if (isResultLargerOrEqual2) score = WIN_FOR_PLAYER_1;
+                    else score = WIN_FOR_PLAYER_2;
+                }
+            }
         }
         else
         {
             for (int i=1; i<3; i++)
             {
                 if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                else { score+= MINUS; tempScore = m_score2;}
                 switch(tempScore)
                 {
                     case 0:
-                        score+="Love";
+                        score+= LOVE;
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score+= FIFTEEN;
                         break;
                     case 2:
-                        score+="Thirty";
+                        score+= THIRTY;
                         break;
                     case 3:
-                        score+="Forty";
+                        score+= FORTY;
                         break;
                 }
             }
