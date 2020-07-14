c267ca6c9a91a237a8da83f37de47248314b013e
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-14 14:05:56 +0700

    update

--- BaiTapPhanLoaiTamGiac/src/triangle/Triangle.java
+++ BaiTapPhanLoaiTamGiac/src/triangle/Triangle.java
@@ -8,7 +8,7 @@ public class Triangle {
     public static final String TAM_GIAC_THUONG = "Tam Giac Thuong";
 
     public static String classifyTriangle(int side1, int side2, int side3){
-        if(areSidesValid(side1, side2, side3)){
+        if(areSidesLargerThanZero(side1, side2, side3)){
             return KHONG_PHAI_TAM_GIAC;
         }
         if(areSidesEqual(side1, side2, side3)){
@@ -35,7 +35,7 @@ public class Triangle {
         return side1==side2&&side2==side3&&side1==side3;
     }
 
-    private static boolean areSidesValid(int side1, int side2, int side3) {
+    private static boolean areSidesLargerThanZero(int side1, int side2, int side3) {
         return side1<=0||side2<=0||side3<=0;
     }
 }
