487fd5deeea832a10bfe70aedfb13a8bbc849d8a
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-14 16:21:15 +0700

    update constant

--- BaiTapFizzBuzz/src/baitapfizzbuzz/FizzBuzz.java
+++ BaiTapFizzBuzz/src/baitapfizzbuzz/FizzBuzz.java
@@ -6,6 +6,8 @@ public class FizzBuzz {
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
     public static final String ERROR = "Error";
+    public static final int MIN_VALUE = 0;
+    public static final int MAX_VALUE = 99;
 
     public static String translateFizzBuzz(int number){
         boolean isDivisibleBy3 = number % 3 == 0;
@@ -13,7 +15,7 @@ public class FizzBuzz {
         Integer numberOBJ= number;
         String numberString=numberOBJ.toString();
 
-        if(number<0||number>99){
+        if(number< MIN_VALUE ||number> MAX_VALUE){
             return numberString;
         }
         if((isDivisibleBy3 && isDivisibleBy5)||(numberString.contains("3")&&numberString.contains("5"))){
