5ee1c8b19c6072017dbecc0c27080e33e8fd82fc
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-14 15:07:36 +0700

    Update FizzBuzz.java

--- BaiTapFizzBuzz/src/baitapfizzbuzz/FizzBuzz.java
+++ BaiTapFizzBuzz/src/baitapfizzbuzz/FizzBuzz.java
@@ -2,21 +2,29 @@ package baitapfizzbuzz;
 
 public class FizzBuzz {
 
+    public static final String FIZZ_BUZZ = "FizzBuzz";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String ERROR = "Error";
+
     public static String translateFizzBuzz(int number){
+        boolean isNumberDivisibleBy3 = number % 3 == 0;
+        boolean isNumberDivisibleBy5 = number % 5 == 0;
         Integer numberOBJ= number;
         String numberStr=numberOBJ.toString();
+
         if(number<0||number>=100){
             return numberStr;
         }
-        if((number%3==0&&number%5==0)||(numberStr.contains("3")&&numberStr.contains("5"))){
-            return "FizzBuzz";
+        if((isNumberDivisibleBy3 && isNumberDivisibleBy5)||(numberStr.contains("3")&&numberStr.contains("5"))){
+            return FIZZ_BUZZ;
         }
-        if(number%3==0||numberStr.contains("3")){
-            return "Fizz";
+        if(isNumberDivisibleBy3 ||numberStr.contains("3")){
+            return FIZZ;
         }
-        if(number%5==0||numberStr.contains("5")){
-            return "Buzz";
+        if(isNumberDivisibleBy5 ||numberStr.contains("5")){
+            return BUZZ;
         }
-        return "Error";
+        return ERROR;
     }
 }
