87ffe6730fe2a64a8c9bd6b2579ba04b9949ddba
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-15 11:19:01 +0700

    add bai tap tinh ngay

--- BaiTapTinhNgayTieptheo/src/nextdaycalculator/NextDayCalculator.java
+++ BaiTapTinhNgayTieptheo/src/nextdaycalculator/NextDayCalculator.java
@@ -1,12 +1,28 @@
 package nextdaycalculator;
-import java.util.Calendar;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.text.ParseException;
-import java.util.Scanner;
+import java.util.*;
 
 public class NextDayCalculator {
-    public static Scanner input=new Scanner(System.in);
-    public static String  date=input.nextLine();
+
+    public Scanner sc = new Scanner(System.in);
+    public String date;
+    public SimpleDateFormat dateFormat;
+    public NextDayCalculator(){
+
+    }
+    public NextDayCalculator (String date,SimpleDateFormat dateFormat) {
+        this.dateFormat = dateFormat;
+        this.date = date;
+
+    }
+
+    public String getNextDate(SimpleDateFormat dateFormat){
+        Date dateParsed = dateFormat.parse(this.date,new ParsePosition(0));
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dateParsed);
+        calendar.add(calendar.DATE,1);
+        return dateFormat.format(calendar.getTime());
+    }
 
 }
